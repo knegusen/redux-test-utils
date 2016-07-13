52599b7643d5f6368432cb90d7df4c8294e89753
Dennis Axelsson <dennis.axelsson@etraveli.com>
2016-07-13 16:50:14 +0200

    Add library target at webpack.config.prod.js to tell webpack it's a module. Change eslint to not use react specific eslint.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,27 +3,28 @@ var webpack = require('webpack');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    devtool: 'source-map',
-    entry: [
-        './src/index.js'
-    ],
-    output: {
-        path: path.join(__dirname, 'lib'),
-        filename: 'index.js'
-    },
-    plugins: [
-    ],
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            loader: 'babel-loader',
-            include: path.join(__dirname, 'src'),
-            query: {
-                presets: ['es2015', 'stage-0', 'react']
-            }
-        }]
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    }
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'lib'),
+    filename: 'index.js',
+    library: 'redux-test-utils',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  plugins: [],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      include: path.join(__dirname, 'src'),
+      query: {
+        presets: ['es2015', 'stage-0']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js']
+  }
 };
